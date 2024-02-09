cc0b1b693237cd271dc0d0ce3895fa5f5ef34b89
Jan Carlo De León <janleoncif11@gmail.com>
2024-02-08 22:28:49 -0600

    Inicio de la implementacion de seguridad con JWT

---
+++ src/main/java/com/apimeteorologica/datosmeteorologicos/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.apimeteorologica.datosmeteorologicos.security.jwt;
+
+/**
+ *
+ * @author Jan Carlo
+ */
+public class AuthTokenFilter {
+    
+}
--- src/main/java/com/apimeteorologica/datosmeteorologicos/security/repository/RoleRepository.java
+++ src/main/java/com/apimeteorologica/datosmeteorologicos/security/repository/RoleRepository.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.apimeteorologica.datosmeteorologicos.security.util;
+package com.apimeteorologica.datosmeteorologicos.security.repository;
 
 /**
  *
  * @author Jan Carlo
  */
-public enum RolEnum {
-    ROLE_USER
+public interface RoleRepository {
+    
 }
